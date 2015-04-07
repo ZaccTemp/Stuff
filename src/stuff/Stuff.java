04ca83c62683f3e42a47ad2b82dade8be78fc8bc
Emanz3011 <Emanz3011@FHC-IC0037286.wrdsb.ca>
2015-04-07 13:43:15 -0400

    Hi bruh

---
+++ src/stuff/Stuff.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stuff;
+
+/**
+ *
+ * @author Emanz3011
+ */
+public class Stuff {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Type here:
+        //Bananaboat
+    }
+}
